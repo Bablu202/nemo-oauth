88981003861a0c085645b089f6deb76216a79b09
aware <aware9758@gmail.com>
2024-04-23 11:54:27 +0200

    23.4-update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -14,14 +14,33 @@ export default async function ProfilePage() {
   const user = session.user;
 
   return (
-    <section className="flex justify-center border ">
-      <div className="mt-8">
-        <p className="mb-3">Id: {user.id}</p>
-        <p className="mb-3">Role: {user.role}</p>
-        <p className="mb-3">Email: {user.email}</p>
-        <p className="mb-3">Provider: {user.app_metadata["provider"]}</p>
-        <p className="mb-3">Created At: {user.created_at}</p>
-        <SignOutButton />
+    <section className="flex justify-center">
+      <div className="max-w-md w-full mt-8 bg-white shadow-md rounded-lg overflow-hidden">
+      
+        <div className="px-6 py-4 flex items-center">
+          <div>
+            <div className="font-bold text-xl mb-2">Profile Information</div>
+            <p className="mb-3">
+              <span className="font-bold">Id:</span> {user.id}
+            </p>
+            <p className="mb-3">
+              <span className="font-bold">Role:</span> {user.role}
+            </p>
+            <p className="mb-3">
+              <span className="font-bold">Email:</span> {user.email}
+            </p>
+            <p className="mb-3">
+              <span className="font-bold">Provider:</span>{" "}
+              {user.app_metadata["provider"]}
+            </p>
+            <p className="mb-3">
+              <span className="font-bold">Created At:</span> {user.created_at}
+            </p>
+          </div>
+        </div>
+        <div className="px-6 py-4">
+          <SignOutButton />
+        </div>
       </div>
     </section>
   );
--- components/actionComponents/SignOut.tsx
+++ components/actionComponents/SignOut.tsx
@@ -10,14 +10,18 @@ const SignOutButton = async () => {
     const supabase = await createSupabaseServerClient();
     await supabase.auth.signOut();
   };
-
+  const info = () => {
+    console.log(data);
+  };
   return (
     <nav className=" flex justify-between container items-center">
       <ul className="flex items-center space-x-4">
         {data.session && (
-          <form action={logoutAction} className="flex">
-            <button className="ml-4 bg-white border p-2 mb-6">Logout</button>
-          </form>
+          <>
+            <form action={logoutAction} className="flex">
+              <button className="ml-4 bg-white border p-2 mb-6">Logout</button>
+            </form>
+          </>
         )}
       </ul>
     </nav>
