801a2a0232b0188647823ab211068af0c9f6af4c
bablu202 <aware9768@gmail.com>
2024-04-26 16:11:58 +0200

    next 13

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
-        <WhatsAppButtonContact />
+        {/* <WhatsAppButtonContact /> */}
         {/* <Footer /> */}
       </body>
     </html>
