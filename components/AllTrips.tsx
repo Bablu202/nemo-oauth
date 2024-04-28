64e981c1bde273d5b0dfcfe2e9cc76cba2a4f209
bablu202 <aware9768@gmail.com>
2024-04-28 17:03:01 +0200

    card

--- components/AllTrips.tsx
+++ components/AllTrips.tsx
@@ -10,51 +10,53 @@ function PostCard(post: Post) {
   return (
     <section className="py-2" id="trips">
       <Link href={post.url} legacyBehavior>
-        <div className="z-[-1] relative border  border-custom-pri border-opacity-30 mr-4 rounded-lg shadow-lg snap-always snap-center cursor-pointer overflow-hidden">
-          <div className="w-80 h-60 container ">
-            <Image
-              className="rounded-t-lg object-fill "
-              src={post.imageURL}
-              alt="Trip Image"
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              priority
-              loading="eager"
-            />
-            <div className=" text-white absolute bg-black/1 w-full h-full">
-              <div className="absolute bg-gradient-to-b from-black/70 to-transparent w-full h-16" />
-              <div className="absolute bottom-0 bg-gradient-to-t from-black/90 to-transparent w-full h-16" />
+        <div className="cursor-pointer">
+          <div className="z-[-1] relative border  border-custom-pri border-opacity-30 mr-4 rounded-lg shadow-lg snap-always snap-center  overflow-hidden">
+            <div className="w-80 h-60 container ">
+              <Image
+                className="rounded-t-lg object-fill "
+                src={post.imageURL}
+                alt="Trip Image"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                priority
+                loading="eager"
+              />
+              <div className=" text-white absolute bg-black/1 w-full h-full">
+                <div className="absolute bg-gradient-to-b from-black/70 to-transparent w-full h-16" />
+                <div className="absolute bottom-0 bg-gradient-to-t from-black/90 to-transparent w-full h-16" />
 
-              {/* both image and text */}
-              <div className="p-2 absolute flex flex-col w-full h-full">
-                <div className="flex justify-between">
-                  <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
-                  <div className="flex flex-col ">
-                    <time
-                      dateTime={post.startDate}
-                      className="mb-1 text-xs text-gray-100"
-                    >
-                      {format(parseISO(post.startDate), "LLLL d, yyyy")}
-                    </time>
-                    <time
-                      dateTime={post.returnDate}
-                      className=" mb-1 text-xs text-gray-100 mr-auto"
-                    >
-                      {format(parseISO(post.returnDate), "LLLL d, yyyy")}
-                    </time>
+                {/* both image and text */}
+                <div className="p-2 absolute flex flex-col w-full h-full">
+                  <div className="flex justify-between">
+                    <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
+                    <div className="flex flex-col ">
+                      <time
+                        dateTime={post.startDate}
+                        className="mb-1 text-xs text-gray-100"
+                      >
+                        {format(parseISO(post.startDate), "LLLL d, yyyy")}
+                      </time>
+                      <time
+                        dateTime={post.returnDate}
+                        className=" mb-1 text-xs text-gray-100 mr-auto"
+                      >
+                        {format(parseISO(post.returnDate), "LLLL d, yyyy")}
+                      </time>
+                    </div>
                   </div>
-                </div>
-                <div className="mt-auto text-sm">
-                  <p>
-                    <strong>Duration:</strong> {post.duration}
-                  </p>
-                  <p>
-                    <strong>Status:</strong> {post.status}
-                  </p>
-                  <p>
-                    <strong>Price:</strong> {post.price}
-                  </p>
+                  <div className="mt-auto text-sm">
+                    <p>
+                      <strong>Duration:</strong> {post.duration}
+                    </p>
+                    <p>
+                      <strong>Status:</strong> {post.status}
+                    </p>
+                    <p>
+                      <strong>Price:</strong> {post.price}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
