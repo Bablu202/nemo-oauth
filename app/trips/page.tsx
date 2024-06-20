7caef5ba28293e02972920f72994dc286eba0b3d
aware <aware9758@gmail.com>
2024-06-20 08:32:35 +0200

    empty

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,14 +1,8 @@
 import AllTrips from "@components/AllTrips";
-import TestCorousel from "@components/TestCorousel";
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      {/* <AllTrips /> */}
-      {/* <TestCorousel /> */}
-    </div>
-  );
+  return <div>{/* <AllTrips /> */}</div>;
 };
 
 export default page;
